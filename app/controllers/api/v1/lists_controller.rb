43f972d3a6a112a4f3e74b91364d33d90d678de4
Bryan Goss <bcgoss@gmail.com>
2017-05-01 20:27:17 -0700

    creates list api

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,3 +1,34 @@
 class Api::V1::ListsController < ApplicationController
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      render json: @list, status: :created
+    else
+      render json: {message: @list.errors.full_messages.join(', ')}, status: :error
+    end
+  end
 
+  def show
+    @list = List.find(params[:id])
+    render json: @list
+  end
+
+  def index
+    @lists = List.all
+    render json: @lists
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update_attributes(list_params)
+      render json: @list, status: :updated
+    else
+      render json: { message: @list.errors.full_messages.join(". ") }, status: :error
+    end
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:name)
+    end
 end
--- spec/request/api_v1_lists_spec.rb
+++ spec/request/api_v1_lists_spec.rb
@@ -3,11 +3,49 @@ require 'rails_helper'
 RSpec.describe List, type: :request do
   context 'good requests' do
     it 'creates a list' do
-      list = attributes_for :list
+      list = attributes_for :list, name: 'List Name'
 
       headers = {"ACCEPTS": 'application/json', "Content-Type": 'application/json' }
       post "/api/v1/lists", params: list.to_json, headers: headers
 
+      expect(response.status).to eq(201)
+
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:name]).to eq "List Name"
+    end
+
+    it 'shows a list' do
+      list = create :list, name: 'List Name'
+  
+      get "/api/v1/lists/#{list.id}"
+
+      expect(response.status).to eq(200)
+      
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json[:name]).to eq 'List Name'
+    end
+
+    it 'shows all lists' do
+      first_list = create :list, name: 'First'
+      second_list = create :list, name: 'Second'
+
+      get '/api/v1/lists'
+
+      expect(response.status).to eq(200)
+      
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(json.first[:name]).to eq 'First'
+      expect(json.last[:name]).to eq 'Second'
+    end
+
+    it 'updates lists' do
+      list = create :list, name: 'Initial name'
+
+      new_data = { list: { name: 'New name' } }
+      headers = { 'ACCEPTS': 'application/json', 'Content-Type': 'application/json' }
+
+      put "/api/v1/lists/#{list.id}", params: new_data.to_json, headers: headers
     end
   end
 end
